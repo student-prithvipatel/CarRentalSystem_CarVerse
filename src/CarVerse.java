e9fedc3a3ca03b319ed0eb0722e5bc0cd4135eb3
student-prithvipatel <prithvitpatel@gmail.com>
2025-07-19 17:33:32 +0530

    admin login method added

--- src/CarVerse.java
+++ src/CarVerse.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class CarVerse {
     public static void main(String[] args) throws SQLException {
         Scanner sc=new Scanner(System.in);
-        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/carrental","root","");
-        System.out.println((con!= null)?"success":"failed");
+        Connection conn = DBConnect.getConnection();
+        Admin admin=new Admin();
         System.out.println("1. Admin login");
         System.out.println("2. User login");
         System.out.println("3. User registration");
@@ -13,7 +13,7 @@ public class CarVerse {
         int choice= sc.nextInt();
         switch (choice){
             case 1:
-
+                admin.adminLogin();
                 break;
             case 2:
                 break;
@@ -22,19 +22,43 @@ public class CarVerse {
         }
     }
 }
+class DBConnect {
+
+    static final String URL = "jdbc:mysql://localhost:3306/carrental"; // replace with your DB name
+    static final String USER = "root";       // replace with your DB username
+    static final String PASSWORD = "";   // replace with your DB password
+
+    // This method will give you the connection wherever you call it
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
 class Car{
 
 }
-class User{
-    void adminLogin(){
+class Buyer{
 
+}
+class Admin{
+    Scanner sc=new Scanner(System.in);
+    void adminLogin()throws SQLException{
+        Connection conn = DBConnect.getConnection();
+        String query = "SELECT * FROM admin WHERE adminname = ? AND password = ?";
+        PreparedStatement pst = conn.prepareStatement(query);
+        System.out.print("Enter admin username: ");
+        pst.setString(1, sc.nextLine());
+        System.out.print("Enter password: ");
+        pst.setString(2, sc.nextLine());
+
+        ResultSet rs = pst.executeQuery();
+
+        if (rs.next()) {
+            System.out.println("✅ Admin login successful!");
+//            Menu.adminMenu();  // continue with admin functions
+        } else {
+            System.out.println("❌ Invalid username or password.");
+        }
     }
-}
-class Buyer extends User{
-
-}
-class Admin extends User{
-
 }
 class Rental{
 
