98e6be3f756b2a9289fcf0cfb06d93253fb60530
Prithvi Patel <prithvitpatel@gmail.com>
2025-08-30 20:17:38 +0530

    Update SimpleHash.java

--- src/carverse/model/SimpleHash.java
+++ src/carverse/model/SimpleHash.java
@@ -1,9 +1,12 @@
 package carverse.model;
 
 public class SimpleHash {
+
+    // Custom simple hash function for passwords
     public static int hash(String password) {
         int hash = 0;
         for (int i = 0; i < password.length(); i++) {
+            // multiply each char by its position (i+1) and add to hash
             hash = (hash + password.charAt(i) * (i + 1)) % 100000;
         }
         return hash;
