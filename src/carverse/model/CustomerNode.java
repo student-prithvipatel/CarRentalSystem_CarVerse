d8e40bc187828d353c5e0eda46c9db4ef87d1755
Prithvi Patel <prithvitpatel@gmail.com>
2025-08-30 20:17:16 +0530

    Update CustomerNode.java

--- src/carverse/model/CustomerNode.java
+++ src/carverse/model/CustomerNode.java
@@ -3,14 +3,16 @@ package carverse.model;
 import java.sql.Date;
 
 public class CustomerNode {
+    // Customer details
     public int id;
     public String name;
     public String email;
     public String phone;
     public String address;
     public Date dob;
-    public CustomerNode next;
+    public CustomerNode next; // Pointer to the next node in the linked list
 
+    // Constructor to initialize a customer node
     public CustomerNode(int id, String name, String email, String phone, String address, Date dob) {
         this.id = id;
         this.name = name;
@@ -18,6 +20,6 @@ public class CustomerNode {
         this.phone = phone;
         this.address = address;
         this.dob = dob;
-        this.next = null;
+        this.next = null; // initially no next node
     }
 }
